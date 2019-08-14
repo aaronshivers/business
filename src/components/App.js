1d94eb2bb49e2fea264e2cd21cddf9b598e0d59e
aaronshivers <aaron@moonmail.org>
2019-08-14 14:40:43 -0500

    added Header and CarouselContent

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import { Container } from 'react-bootstrap'
 import Navigation from './Navigation'
+import Header from './Header'
 
 const App = () => (
   <div>
     <Navigation />
+    <Header />
     <Container>
       <h1 className="display-1 text-center">business</h1>
     </Container>
