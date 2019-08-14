53c16704ceb0dba5dde6214db619bdf9198fb914
aaronshivers <aaron@moonmail.org>
2019-08-14 15:46:33 -0500

    added portfolio component

--- src/components/App.js
+++ src/components/App.js
@@ -2,12 +2,15 @@ import React from 'react'
 import { Container } from 'react-bootstrap'
 import Navigation from './Navigation'
 import Header from './Header'
+import Portfolio from './Portfolio'
 
 const App = () => (
   <div>
     <Navigation />
     <Header />
     <Container>
+      <h1 className="my-4">Welcome to My Business</h1>
+      <Portfolio />
       <h1 className="display-1 text-center">business</h1>
     </Container>
   </div>
