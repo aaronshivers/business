ce16d7dfd4e6a628b46f6dc0a0c163ac70becb0f
aaronshivers <aaron@moonmail.org>
2019-08-14 16:11:19 -0500

    added action and features

---
+++ src/components/Action.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Row, Col } from 'react-bootstrap'
+
+const Action = () => (
+  <Row>
+    <Col md={ 8 }>
+      <p className="display-1 text-center">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestias, expedita, saepe, vero rerum deleniti beatae veniam harum neque nemo praesentium cum alias asperiores commodi.</p>
+    </Col>
+  </Row>
+)
+
+export default Action
--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,8 @@ import { Container } from 'react-bootstrap'
 import Navigation from './Navigation'
 import Header from './Header'
 import Portfolio from './Portfolio'
+import Features from './Features'
+import Action from './Action'
 
 const App = () => (
   <div>
@@ -11,6 +13,9 @@ const App = () => (
     <Container>
       <h1 className="my-4">Welcome to My Business</h1>
       <Portfolio />
+      <Features />
+      <hr />
+      <Action />
       <h1 className="display-1 text-center">business</h1>
     </Container>
   </div>
