127d793b96080e810e9753e75a028e582fb5d098
aaronshivers <aaron@moonmail.org>
2019-08-15 13:18:59 -0500

    setup About Page

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Link, NavLink } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import { LinkContainer } from 'react-router-bootstrap'
 import { Container, Navbar, Nav, NavDropdown } from 'react-bootstrap'
 
