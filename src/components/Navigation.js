48258629bfcda5d87f125931837cdf083a0e63dd
aaronshivers <aaron@moonmail.org>
2019-08-15 07:27:35 -0500

    fixed collapse

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,9 +1,16 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link, NavLink } from 'react-router-dom'
+import { LinkContainer } from 'react-router-bootstrap'
 import { Container, Navbar, Nav, NavDropdown } from 'react-bootstrap'
 
 const Navigation = () => (
-  <Navbar fixed="top" variant="dark" bg="dark" expand="lg" collapseOnSelect>
+  <Navbar
+    fixed="top"
+    variant="dark"
+    bg="dark"
+    expand="lg"
+    collapseOnSelect
+  >
     <Container>
       <Navbar.Brand as={ Link } to="/" className="text-capitalize">
         business
@@ -11,27 +18,59 @@ const Navigation = () => (
       <Navbar.Toggle />
       <Navbar.Collapse>
         <Nav className="text-capitalize ml-auto">
-          <Nav.Link as={ Link } to="/about" className="text-capitalize">about</Nav.Link>
-          <Nav.Link as={ Link } to="/services" className="text-capitalize">services</Nav.Link>
-          <Nav.Link as={ Link } to="/contact" className="text-capitalize">contact</Nav.Link>
+          <LinkContainer to="/about">
+            <Nav.Link className="text-capitalize">about</Nav.Link>
+          </LinkContainer>
+          <LinkContainer to="/services">
+            <Nav.Link className="text-capitalize">services</Nav.Link>
+          </LinkContainer>
+          <LinkContainer to="/contact">
+            <Nav.Link className="text-capitalize">contact</Nav.Link>
+          </LinkContainer>
           <NavDropdown title="portfolio" className="text-capitalize">
-            <NavDropdown.Item as={ Link } to="/portfolio-1-col" className="text-capitalize">1 column portfolio</NavDropdown.Item>
-            <NavDropdown.Item as={ Link } to="/portfolio-2-col" className="text-capitalize">1 column portfolio</NavDropdown.Item>
-            <NavDropdown.Item as={ Link } to="/portfolio-3-col" className="text-capitalize">1 column portfolio</NavDropdown.Item>
-            <NavDropdown.Item as={ Link } to="/portfolio-4-col" className="text-capitalize">1 column portfolio</NavDropdown.Item>
-            <NavDropdown.Item as={ Link } to="/portfolio-item" className="text-capitalize">single portfolio item</NavDropdown.Item>
+            <LinkContainer to="/portfolio-1-col">
+              <NavDropdown.Item className="text-capitalize">1 column portfolio</NavDropdown.Item>
+            </LinkContainer>
+            <LinkContainer to="/portfolio-2-col">
+              <NavDropdown.Item className="text-capitalize">1 column portfolio</NavDropdown.Item>
+            </LinkContainer>
+            <LinkContainer to="/portfolio-3-col">
+              <NavDropdown.Item className="text-capitalize">1 column portfolio</NavDropdown.Item>
+            </LinkContainer>
+            <LinkContainer to="/portfolio-4-col">
+              <NavDropdown.Item className="text-capitalize">1 column portfolio</NavDropdown.Item>
+            </LinkContainer>
+            <LinkContainer to="/portfolio-item">
+              <NavDropdown.Item className="text-capitalize">single portfolio item</NavDropdown.Item>
+            </LinkContainer>
           </NavDropdown>
           <NavDropdown title="blog" className="text-capitalize">
-            <NavDropdown.Item as={ Link } to="/blog-home-1" className="text-capitalize">blog home 1</NavDropdown.Item>
-            <NavDropdown.Item as={ Link } to="/blog-home-2" className="text-capitalize">blog home 2</NavDropdown.Item>
-            <NavDropdown.Item as={ Link } to="/blog-post" className="text-capitalize">blog post</NavDropdown.Item>
+            <LinkContainer to="/blog-home-1">
+              <NavDropdown.Item className="text-capitalize">blog home 1</NavDropdown.Item>
+            </LinkContainer>
+            <LinkContainer to="/blog-home-2">
+              <NavDropdown.Item className="text-capitalize">blog home 2</NavDropdown.Item>
+            </LinkContainer>
+            <LinkContainer to="/blog-post">
+              <NavDropdown.Item className="text-capitalize">blog post</NavDropdown.Item>
+            </LinkContainer>
           </NavDropdown>
           <NavDropdown title="other pages" className="text-capitalize">
-            <NavDropdown.Item as={ Link } to="/full-width" className="text-capitalize">full width page</NavDropdown.Item>
-            <NavDropdown.Item as={ Link } to="/sidebar" className="text-capitalize">sidebar page</NavDropdown.Item>
-            <NavDropdown.Item as={ Link } to="/faq" className="text-uppercase">faq</NavDropdown.Item>
-            <NavDropdown.Item as={ Link } to="/404">404</NavDropdown.Item>
-            <NavDropdown.Item as={ Link } to="/pricing" className="text-capitalize">pricing table</NavDropdown.Item>
+            <LinkContainer to="/full-width">
+              <NavDropdown.Item className="text-capitalize">full width page</NavDropdown.Item>
+            </LinkContainer>
+            <LinkContainer to="/sidebar">
+              <NavDropdown.Item className="text-capitalize">sidebar page</NavDropdown.Item>
+            </LinkContainer>
+            <LinkContainer to="/faq">
+              <NavDropdown.Item className="text-uppercase">faq</NavDropdown.Item>
+            </LinkContainer>
+            <LinkContainer to="/404">
+              <NavDropdown.Item>404</NavDropdown.Item>
+            </LinkContainer>
+            <LinkContainer to="/pricing">
+              <NavDropdown.Item className="text-capitalize">pricing table</NavDropdown.Item>
+            </LinkContainer>
           </NavDropdown>
         </Nav>
       </Navbar.Collapse>
