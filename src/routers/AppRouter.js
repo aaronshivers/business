136665e812b563824e072ff66d11f2fa8cfbccda
aaronshivers <aaron@moonmail.org>
2019-08-16 16:45:15 -0500

    setup portfolio2page

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -5,6 +5,7 @@ import AboutPage from '../pages/AboutPage'
 import ServicesPage from '../pages/ServicesPage'
 import ContactPage from '../pages/ContactPage'
 import Portfolio1Page from '../pages/Portfolio1Page'
+import Portfolio2Page from '../pages/Portfolio2Page'
 import NotFoundPage from '../pages/NotFoundPage'
 import Navigation from '../components/Navigation'
 import Footer from '../components/Footer'
@@ -18,6 +19,7 @@ const AppRouter = () => (
         <Route path="/services" component={ ServicesPage } />
         <Route path="/contact" component={ ContactPage } />
         <Route path="/portfolio-1-col" component={ Portfolio1Page } />
+        <Route path="/portfolio-2-col" component={ Portfolio2Page } />
         <Route component={ NotFoundPage } />
       </Switch>
     <Footer />
